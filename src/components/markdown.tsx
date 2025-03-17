cc148ee7f8104d1c6a9816cf530997cda2a218d8
nonon <nononcrust@gmail.com>
2025-03-17 18:07:43 +0900

    마크다운 기능 수정

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,32 +1,10 @@
-import rehypeShiki from "@shikijs/rehype";
-import { createElement, Fragment } from "react";
-import { jsx, jsxs } from "react/jsx-runtime";
-import rehypeReact from "rehype-react";
-import rehypeSanitize from "rehype-sanitize";
+import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import remarkParse from "remark-parse";
-import remarkRehype from "remark-rehype";
-import { unified } from "unified";
-
-const markdownToReact = async (markdown: string) => {
-  const parsed = await unified()
-    .use(remarkParse, { fragment: true })
-    .use(remarkGfm)
-    .use(remarkRehype)
-    .use(rehypeSanitize)
-    .use(rehypeShiki, { theme: "one-dark-pro" })
-    .use(rehypeReact, { Fragment, createElement, jsx, jsxs })
-    .process(markdown);
-
-  return parsed.result;
-};
 
 type MarkdownProps = {
-  markdown: string;
+  content: string;
 };
 
-export const Markdown = async ({ markdown }: MarkdownProps) => {
-  const content = await markdownToReact(markdown);
-
-  return <>{content}</>;
+export const Markdown = ({ content }: MarkdownProps) => {
+  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
 };
