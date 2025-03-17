59fdd3dda8d485e0fd3720c80792c06c051044aa
nonon <nononcrust@gmail.com>
2025-03-17 18:20:43 +0900

    syntax highlighting 추가

--- src/components/markdown.tsx
+++ src/components/markdown.tsx
@@ -1,10 +1,30 @@
+import { Code } from "bright";
+import { ExternalLinkIcon } from "lucide-react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+
+Code.theme = "one-dark-pro";
+Code.lineNumbers = true;
 
 type MarkdownProps = {
   content: string;
 };
 
 export const Markdown = ({ content }: MarkdownProps) => {
-  return <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      components={{
+        pre: Code,
+        a: ({ children, ...props }) => (
+          <a {...props} target="_blank" rel="noopener noreferrer">
+            {children}
+            <ExternalLinkIcon className="ml-2 inline" size={16} />
+          </a>
+        ),
+      }}
+      remarkPlugins={[remarkGfm]}
+    >
+      {content}
+    </ReactMarkdown>
+  );
 };
