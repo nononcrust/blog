e996d11da692f2a40f980d993594a4c0126832ed
nonon <nononcrust@gmail.com>
2025-04-29 18:35:30 +0900

    404 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="flex h-[calc(100vh-64px)] flex-col items-center justify-center">
+      <h1 className="text-2xl font-semibold">404</h1>
+      <h2 className="text-sub mt-1 text-sm">존재하지 않는 페이지입니다.</h2>
+    </main>
+  );
+}
