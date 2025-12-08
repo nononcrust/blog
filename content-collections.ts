da6713964df569c52bb7f2bcc362d9ff3662b09c
nonon <nononcrust@gmail.com>
2025-12-08 12:09:41 +0900

    @next/third-parties, @content-collections/core, @content-collections/next 범프, zod 패키지 추가

--- content-collections.ts
+++ content-collections.ts
@@ -1,12 +1,13 @@
 import { defineCollection, defineConfig } from "@content-collections/core";
 import { compileMDX } from "@content-collections/mdx";
 import remarkGfm from "remark-gfm";
+import { z } from "zod";
 
 const posts = defineCollection({
   name: "posts",
   directory: "posts",
   include: "**/*.mdx",
-  schema: (z) => ({
+  schema: z.object({
     title: z.string(),
     date: z.string(),
   }),
@@ -14,6 +15,7 @@ const posts = defineCollection({
     const mdx = await compileMDX(context, document, {
       remarkPlugins: [remarkGfm],
     });
+
     return {
       ...document,
       mdx,
